791f2d0ce7f80505db166fd30507e06f3d058753
nzbenji <benlittle53@gmail.com>
2020-01-19 09:14:19 +1300

    added delete

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -40,4 +40,10 @@ class PersonController extends Controller
 
         return new PersonResource($person);
     }
+
+        public function destroy(Person $person) {
+            $person->delete();
+
+            return response()->json();
+    }
 }
